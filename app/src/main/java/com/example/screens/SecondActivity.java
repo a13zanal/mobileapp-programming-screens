306b3e68de2d8171ca3317017dfb27775cfa2666
Zandra Alte <a13zanal@student.his.se>
2020-04-28 13:56:48 +0200

    Add a second activity Add a button in the first activity that starts the second activity

---
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView textView = findViewById(R.id.data);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+            int number = extras.getInt("number");
+            textView.setText(name + " , " + number);
+        }
+
+        Button close = findViewById(R.id.close_second_activity);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View View) {
+                finish();
+            }
+        });
+    }
+}
